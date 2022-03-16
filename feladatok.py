cf28cf060f81469222eee39d71eb880e4baeab84
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-03-16 16:02:25 +0100

    feladatok hétvégére 2. alkalom

---
+++ feladatok.py
@@ -0,0 +1,41 @@
+# 1. feladat:
+# írassátok ki az első, majd a harmadik elemet a listából
+
+my_list = [1, 2, 3, 4, 5]
+
+
+# 2. feladat:
+# adjatok a listához 2 új elemet
+
+my_list = [1, 2, 3, 4]
+
+# 3. feladat:
+# a lista utolsó 3 elemét írassátok ki
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 4. feladat:
+# módosítsátok a lista 2., 4., 6. elemét
+
+my_list = ["Ricsi", "Tibi", "Karcsi", "Laci", "Feri", "Zoli"]
+
+
+# 5. feladat:
+# a 2 listából csináljatok 1 listát
+
+my_list1 = [1, 2, 3, 4, 5]
+
+my_list2 = ["Ricsi", "Tibi", "Karcsi"]
+
+# 6. feladat:
+# string formázással állítsátok elő a következő mondatot:
+
+# Peti jelenleg fejlesztőként dolgozik és br. 700000 Ft-ot keres.
+
+name = "Peti"
+salary = "700000"
+
+# 7. feladat:
+# A megadott listában lévő belső listához adjatok hozzá 2 új nevet
+my_list = [1, 2, 3, 4, ['Peti', 'Karcsi', 'Ricsi']]
+
