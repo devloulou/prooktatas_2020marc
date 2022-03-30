2c3ca2941ccd064592152a5552e2a5ee36a16feb
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-03-30 21:02:20 +0200

    7 alkalom vége

--- 7. alkalom/read_module.py
+++ 7. alkalom/read_module.py
@@ -1,4 +1,16 @@
-file_path = r"C:\WORK\Prooktatás\Prooktatas\7. alkalom\Dracula.txt"
+import os
+
+print(__file__)
+folder_path = os.path.dirname(__file__)
+print(folder_path)
+
+file_path = folder_path + '\'' + 'Dracula.txt'
+file_path = f"{folder_path}\Dracula.txt"
+
+file_path = os.path.join(folder_path, 'Dracula.txt')
+print(file_path)
+
+#file_path = r"C:\WORK\Prooktatás\Prooktatas\7. alkalom\Dracula.txt"
 
 def read_data():
     f = open(file_path, 'r')
@@ -20,16 +32,19 @@ def read_data_by_line():
     return my_data
 
 def write_to_file(file_location):
-    my_list = ['Ricsi', 'ZOli', 'Karcsi', 'Peti']
+    my_list = ['Ricsi', '\n', 'ZOli', '\n', 'Karcsi', '\n', 'Peti']
     f = open(file_location, 'w')
-    for item in my_list:
-        f.write(item)
+    f.writelines(my_list)
+    for item in my_list:        
+        f.write(item + '\n') # csúnya módszer
+        
     f.close()
 
 
 
 if __name__ == '__main__':
     write_to_file('test.txt')
+
     exit()
     read_data_by_line()
 
