22bdbfde42441e6c02ef8bbd71500235569b8614
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-04-04 17:57:38 +0200

    hetvegi feladatok

---
+++ 7. alkalom/feladatok_hetvegere.py
@@ -0,0 +1,14 @@
+# 1. Írj egy Python programot, amely bekér három számot a felhasználótól és kiírja a képernyőre a
+# legkisebb értéket ezek közül!
+
+# 2. Írj egy Python programot, amely bekér három számot a felhasználótól és kiírja a képernyőre a
+# legnagyobb értéket ezek közül!
+
+# 3. Írj egy Python programot, amely bekér egy dolgozat pontszámot (x) a felhasználótól és kiír egy
+# érdemjegyet az alábbiak szerint! 1: x<50; 2: 50<=x<60; 3: 60<=x<70; 4: 70<=x<85; 5: x>=85.
+
+# 4. Írj egy Python programot, amely bekér egy egész számot a felhasználótól és kiírja a képernyőre,
+# hogy osztható-e (igen/nem) a szám 3-mal vagy 5-tel!
+
+# 5. Írj egy Python programot, amely bekér három számot a felhasználótól és kiírja a képernyőre,
+# hogy a számok közül bármelyik kettőnek az összege egyenlő-e a harmadik számmal!
