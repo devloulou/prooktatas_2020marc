9a816f639edc045a4c966f621a924509250c52c3
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-04-06 17:59:05 +0200

    teszt_feladatok

---
+++ 9. alkalom/teszt_feladatok.py
@@ -0,0 +1,16 @@
+# 1. feladat:
+# A Fibonacci sorozatra igaz, hogy minden elem az azt megelőző 2 elem
+# összegével egyenlő.
+# Írjatok egy olyan függvényt, amely paraméterben kap egy számot.
+# A megadott szám a Fibonacci sorozat új elemeinek számát jelenti.
+
+# A függvény szúrja be az új elemeket a lenn megadott listába
+# példa függvényhívásra: my_fib(2) -> elvárt eredmény:  [0,1,1,2,3,5,8,13]
+
+my_list = [0,1,1,2,3,5]
+
+# 2. feladat:
+# Írjatok egy olyan programot, amely
+# a 4 matematikai alapműveletet elvégzi
+# a programot úgy írjátok meg, hogy 3 paraméltere legyen: 2 szám és egy matematikai jel
+
