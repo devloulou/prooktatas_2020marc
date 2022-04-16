c6666f5d561067a252073e2902e76fc76161f25d
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-04-16 11:20:41 +0200

    feladatok

---
+++ 11. alkalom/hétvégi_feladat.py
@@ -0,0 +1,37 @@
+
+#1. Feladat
+#Írj egy programot, amely tartalmaz egy 'osszegzo' nevű függvényt,
+# ami a paraméterként átvett listaelemeket (egész számokat) összeadja és az összegükkel tér vissza!
+# A program tartalmazza a függvény hívását is!
+
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+####################################################################
+# 2. Feladat
+# Írj egy programot, amely tartalmaz egy 'paros_e' nevű függvényt, 
+# amely True értékkel tér vissza, ha a paraméterként átvett listaelemek (egész számok) között van páros, 
+# ellenkező esetben a visszatérési érték False! A program tartalmazza a függvény hívását is!
+
+my_list = [1, 3, 5, 6, 7, 8, 9]
+
+my_list2 = [11, 13, 15, 17, 19, 21]
+
+####################################################################
+# 3.1 Feladat
+# Írj egy programot, amely tartalmaz egy 'harommal_oszthatok' nevű függvényt, 
+# amely a paraméterként átvett listában megvizsgálja, 
+# hogy hány darab hárommal osztható szám van, és ezzel az értékkel tér vissza! 
+# A program tartalmazza a függvény hívását is!
+
+my_list2 = [11, 13, 15, 17, 19, 21]
+
+# 3.2 Feladat
+# Alakítsd át az előző programot úgy, hogy a felhasználó által megadott számokat tárolja el a program egy listában,
+# és ezt értékelje ki a függvény! (Az adatbeolvasás addig tartson, míg a felhasználó negatív számot nem ad meg!)
+
+
+####################################################################
+
+# 4. Feladat
+# Írj egy programot, ami addig kér be egész pozitív számokat, amíg a felhasználó negtív számot nem ír!
+# A megadott számokat tárolja a program egy listában, és ezt adja át paraméterként egy függvények,
+# amely a lista legkisebb elemével tér vissza. A program írja ki, hogy melyik volt a megadott legkisebb szám!
