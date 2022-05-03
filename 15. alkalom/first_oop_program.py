ab1d55d67e67fa17b6d2d9185d0ec46016c05ee9
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2022-05-03 18:35:08 +0200

    oop project

--- 15. alkalom/first_oop_program.py
+++ 15. alkalom/first_oop_program.py
@@ -4,6 +4,9 @@
 
 # @dataclass
 
+import json
+
+
 class Vehicle:
     def __init__(self, name, price):
         self.name = name
@@ -31,6 +34,20 @@ class Catalogue:
         self.cars = {}
         self.motors = {}
 
+    def __str__(self):
+        temp = {
+            'cars': [],
+            'motors': []
+        }
+
+        for key, value in self.cars.items():
+            temp['cars'].append({"brand": key, "price": value.price})
+
+        for key, value in self.motors.items():
+            temp['motors'].append({"brand": key, "price": value.price})
+
+        return json.dumps(temp, indent=4)
+
 class Salon:
     def __init__(self):
         self.base_money = 3000000
@@ -40,25 +57,36 @@ class Salon:
         if vehicle.type == 'car':
            self.catalogue.cars[vehicle.name] = vehicle
         else:
-            self.catalogue.motors[vehicle.name] = vehicle        
+            self.catalogue.motors[vehicle.name] = vehicle
+
+    # törölni az adott autót motort a katalógusból
+    # a törölt object értékével növelni a base_money értékét
+
+    def delete_from_catalogue(self, item):
+        if item.type == "car":
+            self.catalogue.cars.pop(item.name)
+        else:
+            self.catalogue.motors.pop(item.name)
+
+        return item
+    
+    def transaction_handling(self, price):
+        if price > 0:
+            self.base_money += price
+        else:
+            self.base_money -= price
        
 
 volvo = Car(name="Volvo", price=1250000)
 bmw = Car(name="BMW", price=5000000)
-
-print(volvo)
+harley = Motor(name="Harley Davidson", price=10000000)
 
 auto_salon = Salon()
 
 auto_salon.upload_catalogue(bmw)
+auto_salon.upload_catalogue(volvo)
+auto_salon.upload_catalogue(harley)
 
-print(auto_salon)
-print(auto_salon.__str__())
-print(auto_salon.__repr__())
+print(auto_salon.catalogue)
 
-my_num = 5
 
-print(my_num.__str__())
-print(my_num.__repr__())
-
-print(type(my_num))
